9a684f860c295b31a040c5d32da361ac62203754
dylan rojo <devdylancrowder@outlook.com>
2024-07-23 19:32:58 -0300

    fetching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [token, setToken] = useState("");
+
+  const fetching = async () => {
+    const response = await fetch(
+      "https://backend-test-psi-hazel.vercel.app/token"
+    );
+    const data = await response.json();
+    setToken(data.token);
+  };
+
+  useEffect(() => {
+    fetching();
+  }, []);
+
   return (
     <div className="App">
       <h1>hola</h1>
+      <h3>{token}</h3>
     </div>
   );
 }
